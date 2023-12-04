c559cafc258c3760b15c27d2586f2de0dd0d0d5f
Rukaiya Fahmida <rukaiyafahmida7@gmail.com>
2023-12-04 15:11:57 +0600

    Update 2264. Largest 3-Same-Digit Number in String.py

--- 2264. Largest 3-Same-Digit Number in String.py
+++ 2264. Largest 3-Same-Digit Number in String.py
@@ -1,9 +1,13 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
         coll = set()
-        for i in range(len(num)-3):
-            if num[i] == num[i+1] and num[i+1] == num[i+2]:
-                coll.add(num[i])
+        rng = len(num)-2
+        try:
+            for i in range(rng):
+                if num[i] == num[i+1] and num[i+1] == num[i+2]:
+                    coll.add(num[i])
+        except Exception:
+            pass
         if coll:
             return (max(coll))*3
         return ""
