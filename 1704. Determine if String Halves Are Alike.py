be9f95199afb6f7cafe0071450998d0d91e1a426
Rukaiya Fahmida <rukaiyafahmida7@gmail.com>
2024-04-22 13:53:25 +0600

    Update 1704. Determine if String Halves Are Alike.py

--- 1704. Determine if String Halves Are Alike.py
+++ 1704. Determine if String Halves Are Alike.py
@@ -1,14 +1,12 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        l = int(len(s))
-        l2 = l / 2
-        v_list = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+        l = len(s)
+        v_list = "aeiouAEIOU"
         vc_1 = 0
         vc_2 = 0
-        for i in range(l):
+        for i in range(l // 2):
             if s[i] in v_list:
-                if i < l2:
-                    vc_1 += 1
-                else:
-                    vc_2 += 1
+                vc_1 += 1
+            if s[l-i-1] in v_list:
+                vc_2 += 1
         return vc_1 == vc_2
